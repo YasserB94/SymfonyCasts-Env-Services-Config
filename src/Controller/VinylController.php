8fd247f6aba41834a4d4799575cdbfc82ff214a7
Yasser Bal <yasserbal@icloud.com>
2022-09-09 16:04:29 +0200

    Added Http-client

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,7 +35,7 @@ class VinylController extends AbstractController
      * @param  mixed $slug
      * @return Response
      */
-    public function browse(DateTimeFormatter $timeFormatter, string $slug = null): Response
+    public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
         $mixes = $this->getMixes();
