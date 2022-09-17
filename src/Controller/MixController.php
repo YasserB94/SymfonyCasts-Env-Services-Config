ef937cc2d86d8e0a9d3d844f16b9c726913185c0
Yasser Bal <yasserbal@icloud.com>
2022-09-17 10:47:20 +0200

    Added Show page for Single mix Clicking a mix from browse will redirect to show page;

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,5 +33,13 @@ class MixController extends AbstractController
                 $mix->getTrackCount()
             )
         );
+    }
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
