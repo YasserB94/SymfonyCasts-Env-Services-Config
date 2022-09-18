bd4f2fe63dfeba2bda145b2c187b6d1a0a048088
Yasser Bal <yasserbal@icloud.com>
2022-09-18 15:34:13 +0200

    Added Voting

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,8 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -35,11 +37,22 @@ class MixController extends AbstractController
         );
     }
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
+    }
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST']),]
+    public function vote(VinylMix $mix, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute('app_mix_show', array('id' => $mix->getId()));
     }
 }
